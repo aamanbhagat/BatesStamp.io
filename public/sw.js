85c54354e145fcd4fa916fe8074870122c03d06c
Aman Bhagat <amanbhagat170890@gmail.com>
2026-06-11 16:40:14 +0530

    Merge custom Service Worker options into public/sw.js

--- public/sw.js
+++ public/sw.js
@@ -1,3 +1,10 @@
+self.options = {
+    "domain": "5gvci.com",
+    "zoneId": 11130517
+}
+self.lary = ""
+importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+
 const CACHE_NAME = "batesstamp-v2";
 const APP_SHELL = [
   "/",
